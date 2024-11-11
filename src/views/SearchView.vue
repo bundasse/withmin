24960aca87e4efcf48f36f6d87644d021b9600de
bundasse <ymtotolove@gmail.com>
2024-11-11 22:39:11 +0900

    친구검색+추가 페이지

---
+++ src/views/SearchView.vue
@@ -0,0 +1,36 @@
+<script setup>
+import {ref} from 'vue';
+
+const searchText = ref('')
+const dataList = ref([])
+function addFriendCommand(value) {
+  console.log(value)
+}
+</script>
+
+<template>
+  <div>
+    <div class="searchBox">
+      <div class="inputContainer">
+        <input type="text" class="searchBar" v-model="searchText">
+        <button class="searchButton"></button>
+      </div>
+    </div>
+    <div class="searchResults">
+      <ul>
+        <li v-for="(data,i) in dataList" :key="i">
+          <div class="userIcon">
+            <img src="https://placehold.co/50x50/orange/white" alt="usericon">
+          </div>
+          <p>
+            닉네임
+          </p>
+          <button class="addFriend" @click="addFriendCommand(data)">추가</button>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style>
+</style>
